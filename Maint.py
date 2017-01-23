e46f46a0c392e6167c576da122241f5d98296022
cjncjncc <399496787@qq.com>
2017-01-23 09:05:26 +0800

    house spider

--- Maint.py
+++ Maint.py
@@ -4,21 +4,29 @@ import urllib2
 import re
 
 page = 1
-url = 'http://www.qiushibaike.com/hot/page/' + str(page)
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+url = 'http://210.75.213.188/shh/portal/bjjs/index.aspx'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
+
+MonthPattern = re.compile('<!--月统计begin-->.*?<div class="tContent".*?</div>\s*?</div>',re.S)
+StaLiPattern = re.compile('<li style.*?</div>\s*?</li>',re.S)
+AgentPattern = re.compile('<tr>\s*?<td>\d</td>.*?</td>\s*?</tr>',re.S)
 headers = { 'User-Agent' : user_agent }
 try:
     request = urllib2.Request(url,headers = headers)
     response = urllib2.urlopen(request)
-    content = response.read().decode('utf-8')
-    pattern = re.compile('<div.*?author clearfix">.*?<a.*?<img.*?>(.*?)</a>.*?<div.*?'+
-                     'content">.*?<span>(.*?)</span>',re.S)
-    items = re.findall(pattern,content)
-    for item in items:
-        print item[0],item[1]
+    content = response.read()
+    monthStats = re.findall(MonthPattern,content)
+    if len(monthStats)== 0:
+        print 'no month Stats'
+    monthStr = monthStats[0]
+    monthLi = re.findall(StaLiPattern,monthStr)
+    agentResult = re.findall(AgentPattern,monthLi[0])
+    print monthLi[0]
+    print len(agentResult)
 except urllib2.URLError, e:
     if hasattr(e,"code"):
         print e.code
     if hasattr(e,"reason"):
         print e.reason
-
+except Exception, e:
+        print e
